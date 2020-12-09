0fbf8f26543ddb04f8d7ae94b214527cdbe19d5f
Sam Dobson <1309834+samdobson@users.noreply.github.com>
2020-12-09 17:37:08 +0000

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { NxExpertModule } from '@aposin/ng-aquila/config';
+import { NxInputModule } from '@aposin/ng-aquila/input';
+import { NxGridModule } from '@aposin/ng-aquila/grid';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NxExpertModule,
+    NxInputModule,
+    NxGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
